6822a70ddc3e6fb66c0aa352c9fab9eacb944a6f
murdercdh <murdercdh@hotmail.com>
2016-03-01 15:17:18 +0800

    update ci markdown

--- bin/csv_import.js
+++ bin/csv_import.js
@@ -11,6 +11,7 @@ var _ = require('lodash');
 
 converter.transform = function (json, row, index) {
     json["最低首付"] = _.toNumber(_.trimEnd(json["最低首付"], '%'));
+    json["贷款限额"] = _.trimEnd(json["贷款限额"], '万');
     //json["年手续费率"] = _.toNumber(_.trimEnd(json["年手续费率"],'%'));
     /* some other examples:
      delete json["myfield"]; //remove a field
@@ -21,6 +22,8 @@ converter.transform = function (json, row, index) {
 converter.fromFile("../raw/installment.csv", function (err, result) {
     if (err) return console.log(err.message);
     async.each(result, function (re, cb) {
+
+
         var insertSql = 'INSERT INTO cm_installment_new SET ?';
         //'product,' +
         //'institution,' +
@@ -41,11 +44,11 @@ converter.fromFile("../raw/installment.csv", function (err, result) {
         itemArr.product = re['产品名称'];
         itemArr.institution = re['金融机构'];
         itemArr.city = re['城市'];
-        itemArr.down_payment = re['最低首付'];
-        itemArr.interest_rate = re['年手续费'];
+        itemArr.down_payment = parseInt(re['最低首付']) / 100;
+        //itemArr.interest_rate = re['年手续费'];
         itemArr.month_limits = re['分期期限'];
-        itemArr.price_limits = re['贷款限额'];
-        itemArr.rates = re['产品费率/期限'];
+        //itemArr.price_limits = re['贷款限额'];
+        //itemArr.rates = re['产品费率/期限'];
         itemArr.description = re['产品说明'];
         itemArr.requirement = re['申请条件'];
         itemArr.sample = re['例子'];
@@ -53,9 +56,24 @@ converter.fromFile("../raw/installment.csv", function (err, result) {
         itemArr.url = re['更多车源链接'];
         itemArr.fee = re['贷款手续费'];
 
-        db.ExecuteQueryNoCache(insertSql, itemArr, function (err, res) {
-            if (err) return console.log(err.message);
-            console.log(res);
+        var month = re['产品费率/期限'].split('，');
+        var plimit = re['贷款限额'].split('-');
+
+        itemArr.low_price = parseInt(plimit[0]) * 10000;
+        itemArr.high_price = parseInt(plimit[1]) * 10000;
+
+        if (month.length > 3) {
+            return cb(null, null);
+        }
+        async.eachSeries(month, function (mo, cb) {
+            var tpmo = mo.split('年');
+            itemArr.month_limits = parseInt(tpmo[0]) * 12;
+            itemArr.rates = parseInt(_.trimEnd(tpmo[1], '%')) / 100;
+            db.ExecuteQueryNoCache(insertSql, itemArr, cb);
+        }, function (err) {
+            cb();
         });
+    }, function (err) {
+        console.log(err);
     });
 });
